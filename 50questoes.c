c2f1032e7e8d366852760fd58420d45ce167702e
ohsnapitzzcat <133921332+ohsnapitzzcat@users.noreply.github.com>
2024-03-11 14:53:49 +0000

    Update and rename 50questoes to 50questoes.c

---
+++ 50questoes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+
+void ex1() {
+    int maior = INT_MIN;
+    puts("Maior de uma sequência\n Insere uma sequencia de inteiros terminada em 0");
+    while (1) {
+        int num;
+        assert(scanf("%d",&num) == 1);
+        if (num == 0) break;
+        if (num > maior) maior = num;
+    }
+    printf("Maior: %d\n", maior);
+}
