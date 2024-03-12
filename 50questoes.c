dff01d8d2f1e6f74958cfc356f184e9396837899
ohsnapitzzcat <133921332+ohsnapitzzcat@users.noreply.github.com>
2024-03-12 17:26:02 +0000

    Update 50questoes.c

--- 50questoes.c
+++ 50questoes.c
@@ -15,3 +15,34 @@ void ex1() {
     }
     printf("Maior: %d\n", maior);
 }
+
+void ex2(){
+    double soma = 0;
+    int n = 0;
+    puts("Média da sequencia\n Insira uma sequencia de numeros inteiros terminados em 0:");
+    while(1) {
+        int num;
+        assert(scanf("%d",&num) == 1);
+        if (num == 0) break;
+        soma += num;
+        n++;
+    }
+    double media = soma / n;
+    printf("Média: %.5f\n", media);
+}
+
+void ex3(){
+    int max = INT_MIN, segundomax = INT_MIN;
+    puts("Segundo maior\n Insira uma sequencia de numeros terminada em 0:");
+    while (1){
+        int num;
+        assert(scanf("%d", &num) == 1);
+        if (num == 0) break;
+        if (num > max) {
+             segundomax= max;
+             max = num;
+         } else if (num > segundomax) segundomax = num;
+    }
+    printf("Segundo maior numero: %d\n", segundomax);
+}
+
